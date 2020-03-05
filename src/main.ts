d751493e27a953ad028a6817d7ee54a54bb175d5
terukazu inoue <terukazu.inoue@codianz.com>
2020-03-05 14:01:51 +0900

    不要なimportを削除

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import { BootstrapVue, IconsPlugin, LayoutPlugin , ModalPlugin, DropdownPlugin, TablePlugin } from 'bootstrap-vue'
+import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'bootstrap'
